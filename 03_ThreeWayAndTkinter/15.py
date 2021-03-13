240c2314d0fa3bf984f04a674c3b42c002862add
punkevich <punkevich@list.ru>
2021-03-13 23:15:19 +0300

    Creating widgets on window

--- 03_ThreeWayAndTkinter/15.py
+++ 03_ThreeWayAndTkinter/15.py
@@ -4,6 +4,10 @@
 
 import tkinter as tk
 import time
+import random
+
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+random.shuffle(A)
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -12,17 +16,45 @@ class Application(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.timeButton = tk.Button(self, text='Time', command=self.settime)
-        self.timeLabel = tk.Label(self)
-        self.timeButton.grid()
-        self.quitButton.grid(row=0, column=1)
-        self.timeLabel.grid(columnspan=2)
-        self.settime()
+        self.newgameButton = tk.Button(self, text='New', command=self.startnewgame)
+        self.exitButton = tk.Button(self, text='Exit', command=self.quit)
+        self.newgameButton.grid()
+        self.exitButton.grid(row=0, column=1)
+        # cells
+        self.cell_00 = tk.Button(self, text=A.pop(0))
+        self.cell_01 = tk.Button(self, text=A.pop(0))
+        self.cell_02 = tk.Button(self, text=A.pop(0))
+        self.cell_03 = tk.Button(self, text=A.pop(0))
+        self.cell_10 = tk.Button(self, text=A.pop(0))
+        self.cell_11 = tk.Button(self, text=A.pop(0))
+        self.cell_12 = tk.Button(self, text=A.pop(0))
+        self.cell_13 = tk.Button(self, text=A.pop(0))
+        self.cell_20 = tk.Button(self, text=A.pop(0))
+        self.cell_21 = tk.Button(self, text=A.pop(0))
+        self.cell_22 = tk.Button(self, text=A.pop(0))
+        self.cell_23 = tk.Button(self, text=A.pop(0))
+        self.cell_30 = tk.Button(self, text=A.pop(0))
+        self.cell_31 = tk.Button(self, text=A.pop(0))
+        self.cell_32 = tk.Button(self, text=A.pop(0))
+        self.cell_00.grid(row=1, column=0)
+        self.cell_01.grid(row=1, column=1)
+        self.cell_02.grid(row=1, column=2)
+        self.cell_03.grid(row=1, column=3)
+        self.cell_10.grid(row=2, column=0)
+        self.cell_11.grid(row=2, column=1)
+        self.cell_12.grid(row=2, column=2)
+        self.cell_13.grid(row=2, column=3)
+        self.cell_20.grid(row=3, column=0)
+        self.cell_21.grid(row=3, column=1)
+        self.cell_22.grid(row=3, column=2)
+        self.cell_23.grid(row=3, column=3)
+        self.cell_30.grid(row=4, column=0)
+        self.cell_31.grid(row=4, column=1)
+        self.cell_32.grid(row=4, column=2)
 
-    def settime(self):
-        self.timeLabel["text"] = time.strftime("%c")
-
+    def startnewgame(self):
+    	self.createWidgets()
+    	
 app = Application()
-app.master.title('Sample application')
+app.master.title('15')
 app.mainloop()
