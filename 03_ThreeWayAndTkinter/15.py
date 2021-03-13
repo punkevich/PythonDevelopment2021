bbf9d2baeac103c3340139a9525486720a5f64fb
punkevich <punkevich@list.ru>
2021-03-13 22:16:03 +0300

    Added template of program

---
+++ 03_ThreeWayAndTkinter/15.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# simple example
+
+import tkinter as tk
+import time
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.timeButton = tk.Button(self, text='Time', command=self.settime)
+        self.timeLabel = tk.Label(self)
+        self.timeButton.grid()
+        self.quitButton.grid(row=0, column=1)
+        self.timeLabel.grid(columnspan=2)
+        self.settime()
+
+    def settime(self):
+        self.timeLabel["text"] = time.strftime("%c")
+
+app = Application()
+app.master.title('Sample application')
+app.mainloop()
